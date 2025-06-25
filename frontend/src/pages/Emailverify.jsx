435012c5b4cb52aa80d4658a1de978be72d6c084
ArpithP-sys <arpithpoojary956@gmail.com>
2025-06-25 11:10:31 +0530

    Updated

--- frontend/src/pages/Emailverify.jsx
+++ frontend/src/pages/Emailverify.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useRef, useState } from 'react';
-import { AppContent } from '../context/appcontext';
+import { AppContent } from '../context/AppContext';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast';
