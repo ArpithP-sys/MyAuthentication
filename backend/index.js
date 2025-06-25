38a152d7582907294dd740f602e0696e8ed4ab0c
ArpithP-sys <arpithpoojary956@gmail.com>
2025-06-25 11:04:28 +0530

    Updated for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,22 +7,35 @@ import authrouter from "./routes/authroutes.js";
 import { setMaxListeners } from 'events';
 
 import userRouter from "./routes/userRoutes.js";
+import path from "path";
 
 setMaxListeners(20); 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const __dirname = path.resolve();
 
 const allowedOrigins=['http://localhost:5173'];
 connectDB()
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:allowedOrigins, credentials: true }));
+if(process.env.NODE_ENV==="production"){
+  app.use(cors({origin:allowedOrigins, credentials: true }));
+}
+
 
 // Api endpoint
 app.get('/', (req, res) => res.send("API Working"));
 app.use('/api/auth', authrouter);
 app.use('/api/user',userRouter)
+
+
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));});
+}
 
 // Server listen
  app.listen(PORT, () => {
